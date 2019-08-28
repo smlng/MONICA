9417125875550111cd2a427732f0c6888e3a7ff7
Sebastian Meiling <s@mlng.net>
2019-08-28 08:54:29 +0200

    blimp: update datastreams for WT demo

--- blimp/config.h
+++ blimp/config.h
@@ -13,10 +13,20 @@
 #define CONFIG_PROXY_ADDR    "fe80::1ac0:ffee:c0ff:ee42"
 #define CONFIG_PROXY_PORT    "5683"
 
+
+#if 0
+/* datastreams for Kappa Future Festival (KFF) MONICA demo */
 #define CONFIG_PATH_LOCATION    "/Datastreams(66)/Observations"
 #define CONFIG_PATH_HUMITIDY    "/Datastreams(67)/Observations"
 #define CONFIG_PATH_PRESSURE    "/Datastreams(68)/Observations"
 #define CONFIG_PATH_TEMPERATURE "/Datastreams(69)/Observations"
+#else
+/* datastreams for Woodstower (WT) MONICA demo */
+#define CONFIG_PATH_LOCATION    "/Datastreams(112)/Observations"
+#define CONFIG_PATH_HUMITIDY    "/Datastreams(113)/Observations"
+#define CONFIG_PATH_PRESSURE    "/Datastreams(114)/Observations"
+#define CONFIG_PATH_TEMPERATURE "/Datastreams(115)/Observations"
+#endif
 
 /* LoRaWAN application configurations */
 #define LORAWAN_BUF_SIZE                (64U)
